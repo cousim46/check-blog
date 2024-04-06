bcc2a9189ccf2ac85b1e594e754807b41b58a3b1
cousim46 <wjdghldns24@naver.com>
2024-04-06 23:23:38 +0900

    feat : 블로그 인터페이스 정의

---
+++ src/main/java/org/example/blog/service/blog/BlogService.java
@@ -0,0 +1,21 @@
+package org.example.blog.service.blog;
+
+import java.time.LocalDate;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+
+public interface BlogService {
+    default String getContent(String blogUrl)  {
+        try {
+            Connection connect = Jsoup.connect(blogUrl);
+            return connect.get().body().toString();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    boolean isBetween7DaysBlogWrite(String blogUrl,LocalDate now);
+}
